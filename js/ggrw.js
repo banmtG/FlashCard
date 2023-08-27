27d6e48eb48362e36affb18592f29e9fec3ae88f
banmtG <banmtG@gmail.com>
2023-08-27 21:58:26 +0700

    a

--- js/ggrw.js
+++ js/ggrw.js
@@ -1,6 +1,7 @@
-// https://script.google.com/macros/s/AKfycby2ZfCPuQ4r6TGDLNhWsuW0_qgZ0up5I1Y5or-9p5U05O_gaofuzZ2-JMlaGBkbEMIZ/exec
+// https://script.google.com/macros/s/AKfycbx21yVPtcGsQZEc1S3uboGfO6v3s1qxmHDi_lSRbHXZQxmA5BdLAGkQWSCtgbtmpJI/exec
 
-// AKfycby2ZfCPuQ4r6TGDLNhWsuW0_qgZ0up5I1Y5or-9p5U05O_gaofuzZ2-JMlaGBkbEMIZ
+// AKfycbx21yVPtcGsQZEc1S3uboGfO6v3s1qxmHDi_lSRbHXZQxmA5BdLAGkQWSCtgbtmpJI
+
 
 const data = ["Value1", "Value2", "Value3"]; // Example data to write
 
