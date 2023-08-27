e244d126b4484e53096b9dcb53f165aee22f50fc
banmtG <banmtG@gmail.com>
2023-08-27 17:04:59 +0700

    a

---
+++ js/ggrw.js
@@ -0,0 +1,22 @@
+// https://script.google.com/macros/s/AKfycby2ZfCPuQ4r6TGDLNhWsuW0_qgZ0up5I1Y5or-9p5U05O_gaofuzZ2-JMlaGBkbEMIZ/exec
+
+// AKfycby2ZfCPuQ4r6TGDLNhWsuW0_qgZ0up5I1Y5or-9p5U05O_gaofuzZ2-JMlaGBkbEMIZ
+
+const data = ["Value1", "Value2", "Value3"]; // Example data to write
+
+document.getElementById('Write_to_google').addEventListener('click', () => {
+  fetch('https://script.google.com/macros/s/AKfycby2ZfCPuQ4r6TGDLNhWsuW0_qgZ0up5I1Y5or-9p5U05O_gaofuzZ2-JMlaGBkbEMIZ/exec', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ data }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Data written successfully:', data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
